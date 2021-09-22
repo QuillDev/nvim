6206e110d270f8b2f08b3c71d736b2544af09e2c
Robert Brunson <siinspam@gmail.com>
2021-09-22 16:08:29 -0400

    updatged

--- pack/onedark/test.java
+++
@@ -1,8 +0,0 @@
-public class Test{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.printf("This is the end");
-        System.out.println("Hello World");
-        System.out.println("This does feela  lot more like an ide");
-    }
-}
